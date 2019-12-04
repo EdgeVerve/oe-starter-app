c6814b6b78159ddd475644c6738cc5d2f22ddbd5
Sumeet <sumeet.kumar@edgeverve.com>
2019-12-04 15:17:13 +0530

    updated templates and index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const Generator = require('oe-generator');
 const path = require('path');
 const fullname = require('fullname');
+const merge = require('merge-package-json');
+var appListArray = [];
 module.exports = class extends Generator {
   initializing() {
     var done = this.async();
@@ -19,13 +21,14 @@ module.exports = class extends Generator {
   }
 
   prompting() {
+    debugger;
     return this.prompt([
       {
         type: 'list',
         name: 'oeCloud',
-        message: 'which oeCloud version you want to use?',
-        default: '2.0',
-        choices: ['1.0', '2.0']
+        message: 'Select which type of application you want to use?',
+        default: 'oe-cloud-2.x',
+        choices: ['oe-cloud-1.x', 'oe-cloud-2.x']
       }, {
         type: 'input',
         name: 'version',
@@ -41,13 +44,53 @@ module.exports = class extends Generator {
         name: 'author',
         message: 'author',
         default: this.options.author
-      }
+      },
+	  {
+      type: "checkbox",
+      name: "modules",
+      message: "Up/Down to navigate and Space bar to select oe-cloud modules you want to use in your app.", 
+      choices: ["oe-multi-tenancy: Enables multi-tenancy",
+       "oe-component-passport: Enables passport authentication for third party support, JWT, oAuth etc",
+       "oe-common-mixins: Enables commonly used functionalities such as Version Mixin, Audit Field Mixin, Soft Delete Mixin, etc which can be attached to models",
+       "oe-model-composite: Enables user to combine non related models and do similar operation you do with single model",
+       "oe-personalization: Enables personalize records",
+       "oe-expression: Enables expression language functionality in oecloud that could be used further for validations.",
+       "oe-business-rule: Enables business rule functionality",
+       "oe-validation: Enables validations to be attached to models like Property level, Embedded Model, Relation Validations etc.",
+       "oe-node-red: Enables Node-RED integration with oe-Cloud",
+       "oe-master-job-executor: Enables to run a function once in a randomly selected master app-instance (from a cluster).",
+       "oe-job-scheduler: Enables to schedule jobs based on business rules for scheduling dates and times.",
+       "oe-metadata-ui: Enables support for holding and serving metadata used by oe-ui framework.",
+       "oe-workflow: Enables workflow",
+       "oe-migration: Enables migration",
+       "oe-studio-service: Enables the usage of oe-studio in the application",
+       "oe-batch-processing: Enables to load data into the application database from flat (text) files"
+      ]
+      },
     ]).then((answers) => {
       this.options.author = answers.author || this.options.author;
       this.options.version = answers.version;
       this.options.description = answers.description;
       this.options.oeCloud = answers.oeCloud;
-      this.options.bowerInstall = answers.oeCloud === '1.0' ? true : false;
+
+  
+    var list = answers.modules;
+    var moduleListObject ={};
+      for(var i = 0;i<list.length;i++){
+      var key = list[i].split(':')[0];
+       var value = "git+http://evgit/oecloud.io/"+list[i].split(':')[0] + ".git#master";
+        moduleListObject[key] = value;
+        var appListObject = {};
+        appListObject["path"] = list[i];
+        appListObject["enabled"] = true;
+        appListArray.push(appListObject);
+        key = '';
+        value='';
+      }
+      let dependencyObj = {"dependencies": moduleListObject};
+      this.options.modules = dependencyObj;
+
+      this.options.bowerInstall = answers.oeCloud === 'oe-cloud-1.x' ? true : false;
     });
   }
 
@@ -57,15 +100,23 @@ module.exports = class extends Generator {
       this.templatePath(version + '/common'),
       this.destinationPath('common')
     );
+    if (this.options.oeCloud === 'oe-cloud-2.x'){
+      this.fs.copy(
+        this.templatePath('oe-cloud-2.x/client'),
+        this.destinationPath('client')
+      );
+    }
+     else {
     this.fs.copy(
-      this.templatePath('1.0/client'),
+      this.templatePath('oe-cloud-1.x/client'),
       this.destinationPath('client')
     );
+  }
     this.fs.copyTpl(
       this.templatePath(version + '/server'),
       this.destinationPath('server')
     );
-    if (this.options.oeCloud === '2.0') {
+    if (this.options.oeCloud === 'oe-cloud-2.x') {
       this.fs.copyTpl(
         this.templatePath(version + '/test'),
         this.destinationPath('test')
@@ -79,6 +130,38 @@ module.exports = class extends Generator {
       this.templatePath(version + '/settings'),
       this.destinationPath('./')
     );
+    if (this.options.oeCloud === 'oe-cloud-2.x') {
+    this.fs.copyTpl(
+      this.templatePath(version + '/polymer.json'),
+      this.destinationPath('polymer.json')
+    );
+  }
+
+  if (this.options.oeCloud === 'oe-cloud-2.x') {
+    let existingappList= this.fs.readJSON(
+      this.destinationPath('server/app-list.json')
+    );
+   let list= existingappList.concat(appListArray);
+    this.fs.writeJSON(
+      this.destinationPath('server/app-list.json'),
+      list
+     );
+
+  }
+  if (this.options.oeCloud === 'oe-cloud-2.x') {
+    var existingDataSources= this.fs.readJSON(
+      this.destinationPath('server/datasources.json')
+    );
+    var updatedDataSources = JSON.stringify(existingDataSources)
+    var re = new RegExp('commondb', 'g');
+    updatedDataSources = updatedDataSources.replace(re,this.options.appName);
+    this.fs.writeJSON(
+      this.destinationPath('server/datasources.json'),
+      JSON.parse(updatedDataSources)
+     );
+
+}
+    
     this.fs.copyTpl(
       this.templatePath(version + '/package.json'),
       this.destinationPath('package.json'), {
@@ -88,6 +171,20 @@ module.exports = class extends Generator {
         author: this.options.author
       }
     );
+    if (this.options.oeCloud === 'oe-cloud-2.x') {
+  
+   let existingPkg= this.fs.readJSON(
+      this.destinationPath('package.json')
+    );
+ 
+    existingPkg = merge(existingPkg,this.options.modules);
+  
+   this.fs.writeJSON(
+      this.destinationPath('package.json'),
+     JSON.parse(existingPkg)
+     );
+    }
+
   }
 
   install() {
